fixed sign and direction of bdw loci

non_package_sandbox/bdw_comparison/bdw.py
@@ -167,6 +167,10 @@ class BDW(object):
             #Get midpoint edge locations and edge segment lengths
             self.loci, self.dls = self.build_starshade()
 
+        #Flip x-axis loci define looking down on the occulter, but we are looking from the backside
+        self.loci[:,0] *= -1
+        self.dls[:,0] *= -1
+
     def get_midpoint_scheme(self, loci):
         #Rollover 1 point
         locr = np.concatenate((loci[1:], loci[:1]))
@@ -430,8 +434,8 @@ class BDW(object):
         Emap *= np.exp(1j*self.kk/2 * ap_opd)
         Emap *= np.exp(1j*self.kk * self.z1)
 
-        #Divide by numerical constants (negative sign to match lotus)
-        Emap /= -self.z1 * 4.*np.pi
+        #Divide by numerical constants
+        Emap /= self.z1 * 4.*np.pi
 
         return Emap
 

non_package_sandbox/bdw_comparison/test_bdw.py
@@ -47,6 +47,7 @@ class Test_BDW(object):
                 'with_mask':        True,
                 'do_save':          False,
                 'verbose':          False,
+                'skip_image':       True,
         }
 
         #Load BDW
@@ -65,7 +66,7 @@ class Test_BDW(object):
                 bdw.tel_shift = shift
 
                 #Run BDW simulation
-                bsol = bdw.run_sim()
+                bsol, dummy = bdw.run_sim()
 
                 #Get analytic solution
                 ss = np.hypot(bdw.tel_pts_x, bdw.tel_pts_y[:,None])
@@ -97,6 +98,7 @@ class Test_BDW(object):
             pms['verbose'] = False
             pms['image_pad'] = 0
             pms['with_mask'] = True
+            pms['skip_image'] = True
             waves = pms.pop('waves')
 
             #Loop over wavelengths and run sim
@@ -105,7 +107,7 @@ class Test_BDW(object):
                 #Run Sim
                 pms['wave'] = wave
                 bdw = BDW(pms)
-                emap = bdw.run_sim()
+                emap, dummy = bdw.run_sim()
 
                 #Compare to data
                 cmap = imgs[f'pupil_{wave*1e9:.0f}']