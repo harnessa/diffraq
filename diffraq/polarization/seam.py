fixed problem with seam petals and sign in sommerfeld

diffraq/polarization/seam.py
@@ -80,8 +80,8 @@ class Seam(object):
         #Get petal signs and angle to rotate
         ones = np.ones(self.shape.theta_nodes, dtype=int)
         pet_mul = np.tile(np.concatenate((ones, -ones)), self.shape.num_petals)
-        pet_add = np.repeat(np.roll(np.arange(self.shape.num_petals) + 1, -1), \
-            2*self.shape.theta_nodes)
+        pet_add = 2*(np.repeat(np.roll(np.arange(self.shape.num_petals) + 1, -1), \
+            2*self.shape.theta_nodes) - 1)
 
         #Get function and derivative values at the parameter values
         func = self.shape.outline.func(indt_values)*pet_mul + pet_add

diffraq/polarization/thick_screen.py
@@ -108,8 +108,8 @@ class ThickScreen(object):
             Gv = pre*np.exp(-1j*vv**2.)*F_func(vv)
 
             #Get field solution (minus incident field) for s,p polarization
-            Us = Umid - Gv
-            Up = Umid + Gv
+            Us = Umid + Gv
+            Up = Umid - Gv
 
             #Cleanup
             del U0, rho, phi, uu, vv, pre, Umid, Gv

tests/test_vector.py
@@ -173,13 +173,13 @@ class Test_Vector(object):
 
     def test_petal(self):
         #Test HG
-        r0, r1 = 12, 16
+        r0, r1 = 11, 14
         hga, hgb, hgn = 8,5, 6
         num_pet = 12
         petal_func = lambda r: np.exp(-((r - hga)/hgb)**hgn)
         petal_diff = lambda r: petal_func(r) * (-hgn/hgb)*((r-hga)/hgb)**(hgn-1)
 
-        etch = 0.005
+        etch = 0.01
         seam_width = 2.*abs(etch)             #run larger than etch b/c not normal to edge
 
         #Sim params
@@ -236,6 +236,11 @@ class Test_Vector(object):
         #Compare areas
         varea = vec_area + seam_area
         assert(np.abs(varea - etch_area) < 5e-3)
+
+        # import matplotlib.pyplot as plt;plt.ion()
+        # fs = area_func(ds)
+        # plt.colorbar(plt.scatter(xs, ys, c=fs, s=1))
+        # breakpoint()
 
 ############################################
 ############################################