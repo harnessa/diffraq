fixed sign on sine waves

diffraq/geometry/perturbations/sines.py
@@ -13,6 +13,7 @@ Description: Class of the sine wave perturbation.
 
 import numpy as np
 import diffraq.quadrature as quad
+from scipy.optimize import newton
 
 class Sines(object):
 
@@ -231,7 +232,7 @@ class Sines(object):
         yq = (pr*np.sin(tt)).ravel()
 
         #Get weights rdr = wr*pr, dtheta = ww*dt
-        wq = self.parent.opq_sign * (pr * wr * ww * dt).ravel()
+        wq = -self.parent.opq_sign * (pr * wr * ww * dt).ravel()
 
         #Cleanup
         del pw, ww, pr, wr, old_edge, new_edge, oldt, newt, newr, dt, tt
@@ -314,8 +315,9 @@ class Sines(object):
         x0 = ts.min()*cos_theta(ts.min())
         x1 = ts.max()*cos_theta(ts.max())
         xs = np.linspace(x0, x1, len(ts))
-        #turn xs into rs (use ts as rough guess of r in theta. should really use newton)
-        ur = xs/cos_theta(ts)
+        #turn xs into rs
+        min_diff = lambda r: r - xs/cos_theta(r)
+        ur = newton(min_diff, ts, full_output=False)
         #uniform cycle nodes
         uxx = 2*self.num_cycles*np.linspace(0, 1, len(ts))
         #interpolated cycle nodes at location of loci r values

non_package_sandbox/focuser/check_resize.py
@@ -6,9 +6,10 @@ from scipy.ndimage import affine_transform, shift
 from PIL import Image
 import cv2
 import time
+import h5py
 
 def run_sim():
-    wave = [400e-9, 641e-9][1]
+    wave = [400e-9, 641e-9, 725e-9][2]
     num_pts = 512
     tel_diameter = 5e-3
     min_padding = 4
@@ -17,7 +18,7 @@ def run_sim():
 
     down_sample = [False, True][1]
 
-    msamp = 2.8
+    msamp = 2.1
 
     if down_sample:
         NN = num_img * min_padding * int(np.ceil(msamp))    #number to run (make sure target is also min padded for truth calc)
@@ -28,6 +29,7 @@ def run_sim():
 
     #new image res
     image_res = num_pts * wave / (tel_diameter * targ_NN)
+
     focal_length = 13e-6/image_res
     image_distance = focal_length
 
@@ -36,7 +38,10 @@ def run_sim():
     #################################################
 
     #Get pupil
-    pupil = np.ones((num_pts, num_pts)) + 0j
+    # pupil = np.ones((num_pts, num_pts)) + 0j
+    mask = 'm12p9'
+    with h5py.File(f'./pupil__{mask}__mask_1a.h5', 'r') as f:
+        pupil = f['field'][3]
 
     NN0 = pupil.shape[-1]
     pupil, NN_full = image_util.round_aperture(pupil)
@@ -70,42 +75,43 @@ def run_sim():
 
     #Finalize PIL
     tik2 = time.perf_counter()
-    pimg, pdx = finalize_cv2(img.copy(), num_img, targ_NN, NN, dx)
+    cimg, cdx = finalize_cv2(img.copy(), num_img, targ_NN, NN, dx)
     tok2 = time.perf_counter()
-    print(f'PIL: {tok2-tik2:.3f}')
+    print(f'CV2: {tok2-tik2:.3f}')
 
     #Get integral targ_NN
     Ntt = int(np.round(targ_NN))
     Ntt += Ntt % 2
+
     #Get truth (not really truth with fractional targ_NN b/c we must make integer)
     tru_pupil = image_util.pad_array(tru_pupil, Ntt)
     dxtru = wave*image_distance/(dx0*Ntt)
     xxtru = (np.arange(Ntt)/Ntt - 0.5) * Ntt
     irdx = image_res * image_distance   #True image dx
 
-    #Propage tru image
+    #Propagate tru image
     tru = do_prop(tru_pupil, num_img, dxtru, xxtru, wave, image_distance, NN_full)
     tru = image_util.crop_image(tru, None, num_img//2)
 
     #Difference
     adif = np.abs(tru - aimg).sum()
-    pdif = np.abs(tru - pimg).sum()
+    cdif = np.abs(tru - cimg).sum()
 
-    print(f'Adif: {adif:.3e}, Pdif: {pdif:.3e}')
-    print(f'Adx: {adx-irdx:.3e}, Pdx: {pdx-irdx:.3e}, True dx: {dxtru-irdx:.3e}')
+    print(f'Adif: {adif:.3e}, Cdif: {cdif:.3e}')
+    print(f'Adx: {adx-irdx:.3e}, Cdx: {cdx-irdx:.3e}, True dx: {dxtru-irdx:.3e}')
 
     #Plot
     pxx = xx[NN//2-len(aimg)//2:NN//2+len(aimg)//2]
     pxt = xxtru[Ntt//2-len(tru)//2:Ntt//2+len(tru)//2]
     plt.figure()
     plt.semilogy(pxx, aimg[len(aimg)//2], '-',  label='AFF')
-    plt.semilogy(pxx, pimg[len(pimg)//2], '--', label='PIL')
+    plt.semilogy(pxx, cimg[len(cimg)//2], '--', label='PIL')
     plt.semilogy(pxt, tru[len(tru)//2], ':', label='tru')
     plt.axvline(1.22*wave/tel_diameter/image_res, color='k', linestyle=':')
     plt.axvline(0, color='c', linestyle=':')
     plt.legend()
     plt.xlim([-20,20])
-    plt.ylim(bottom=1e-5)
+    # plt.ylim(bottom=1e-5)
     breakpoint()
 
 def do_prop(pupil, num_img, dx, xx, wave, image_distance, NN_full):

non_package_sandbox/sines/get_sine_params_from_loci.py
@@ -6,7 +6,7 @@ loci_dir = '/home/aharness/repos/Milestone_2/diffraq_analysis/modeling/M12P9/mak
 
 nom = np.genfromtxt(f'{loci_dir}/nompetal12p.txt', delimiter='  ')
 
-use_inn = [False, True][0]
+use_inn = [False, True][1]
 
 if use_inn:
     pet = np.genfromtxt(f'{loci_dir}/petal4_sine12p.txt', delimiter='  ')
@@ -39,7 +39,7 @@ pet = scale(pet, 12/16)
 
 rads = np.hypot(*nom.T)
 
-breakpoint()
+# breakpoint()
 norms = np.diff(nom, axis=0)
 norms = np.vstack((norms[0], norms))
 # norms = util.compute_derivative(nom)
@@ -57,7 +57,7 @@ npts = np.count_nonzero(dd)
 
 the = np.zeros(len(dd))
 
-freq = ncycles/nom[:,0][dd!=0].ptp()
+freq = ncycles/rads[dd!=0].ptp()
 
 # the[dd != 0] = 2.*np.pi*ncycles/npts * np.arange(npts)
 the[dd != 0] = 2.*np.pi*ncycles*(nom[:,0][dd!=0]-nom[:,0][dd!=0].min())/nom[:,0][dd!=0].ptp()

non_package_sandbox/sines/match_loci.py
@@ -31,12 +31,14 @@ use = nom.copy()
 #Build sine wave
 dd = np.hypot(*(pet - nom).T)
 the = np.zeros(len(dd))
-inds = np.where(dd != 0)[0]
+# inds = np.where(dd != 0)[0]
+inds = np.where(abs(dd)>1e-8)[0]
 
 if use_inn:
     inds = inds[:-2]
 else:
-    inds = inds[:-7]
+    # inds = inds[:-7]
+    inds = inds[:-8]
 npts = len(inds)
 
 use = use[inds]

non_package_sandbox/sines/test_build.py
@@ -6,8 +6,8 @@ from scipy.interpolate import interp1d
 from utilities import util
 from scipy.optimize import newton
 
-use_inn = [False, True][1]
-use_raw = [False, True][1]
+use_inn = [False, True][0]
+use_raw = [False, True][0]
 
 if use_inn:
     ncycles = 4