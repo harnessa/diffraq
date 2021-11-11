angspec started to work, need to fix scaling

diffraq/simulator.py
@@ -226,12 +226,14 @@ class Simulator(object):
             yq = self.occulter.yq
             xoff = 0
 
+        #Occulter extent
+        Dmax = np.max([2*shp.max_radius for shp in self.occulter.shapes])
+
         #Run diffraction calculation over wavelength
         for iw in range(len(self.waves)):
 
-            #lambda * z
-            lamzz = self.waves[iw] * self.zz
-            lamz0 = self.waves[iw] * self.z0
+            #Calculate Input field
+            u0 = np.exp(1j*2.*np.pi/self.waves[iw] * (xq**2 + yq**2))
 
             #Apply input beam function
             if self.beam_function is not None:
@@ -241,11 +243,11 @@ class Simulator(object):
                 wq = self.occulter.wq
 
             #Calculate diffraction
-            uu = diffraq.diffraction.diffract_grid(xq, yq, wq, lamzz, grid_pts, \
-                self.fft_tol, lamz0=lamz0, is_babinet=self.occulter.is_babinet)
+            uu = diffraq.diffraction.diffract_angspec(xq, yq, wq, u0, Dmax, \
+                self.waves[iw], self.zz, grid_pts, self.fft_tol, is_babinet=self.occulter.is_babinet)
 
             #Account for extra phase added by off_axis
-            uu *= np.exp(1j*np.pi/lamz0*self.z_scl * xoff)
+            uu *= np.exp(1j*np.pi/(self.waves[iw]*self.z0)*self.z_scl * xoff)
 
             #Multiply by plane wave
             uu *= np.exp(1j * 2*np.pi/self.waves[iw] * self.zz)

tests/test_calc_pupil_field.py
@@ -18,7 +18,7 @@ class Test_calc_pupil_field(object):
 
     def test_pupil_field(self):
 
-        ngrid = 20
+        ngrid = 512
         grid_width = 3
         fresnums = [10,100,1000]
         shapes = ['circle', 'polar']
@@ -53,13 +53,19 @@ class Test_calc_pupil_field(object):
             for i in range(len(fresnums)):
 
                 #Theoretical value
-                utru = diffraq.utils.solution_util.direct_integration( \
-                    fresnums[i], pupil[i].shape, sim.occulter.xq, sim.occulter.yq, \
-                    sim.occulter.wq, grid_2D)
+                # utru = diffraq.utils.solution_util.direct_integration( \
+                    # fresnums[i], pupil[i].shape, sim.occulter.xq, sim.occulter.yq, \
+                    # sim.occulter.wq, grid_2D)
 
                 #Assert max difference is close to specified tolerance
                 max_diff = tol * fresnums[i]
-                assert(np.abs(utru - pupil[i]).max() < max_diff)
+                # assert(np.abs(utru - pupil[i]).max() < max_diff)
+                import matplotlib.pyplot as plt;plt.ion()
+                # plt.figure()
+                # plt.imshow(abs(utru))
+                plt.figure()
+                plt.imshow(abs(pupil[i]))
+                breakpoint()
 
         #Cleanup
         sim.clean_up()